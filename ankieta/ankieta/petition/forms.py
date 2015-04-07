ddb08cfc0f5872e95373b13f3bd9688e426b68d3
Adam Dobrawy <naczelnik@jawnosc.tk>
2015-04-07 23:43:07 +0200

    Added fields newsletter and read only recipient, petition fields

--- ankieta/ankieta/petition/forms.py
+++ ankieta/ankieta/petition/forms.py
@@ -7,9 +7,27 @@ from django.core.urlresolvers import reverse
 from .models import Signature
 
 
+class ReadonlyMixin(object):
+    def __init__(self, *args, **kwargs):
+        super(ReadonlyMixin, self).__init__(*args, **kwargs)
+        self.attrs['readonly'] = True
+
+
+class ReadonlyTextarea(ReadonlyMixin, forms.Textarea):
+    pass
+
+
+class ReadonlyTextInput(ReadonlyMixin, forms.TextInput):
+    pass
+
+
 class SignatureForm(UserKwargModelFormMixin, ModelForm):
+    recipient = forms.CharField(widget=ReadonlyTextInput(), initial="A")
+    petition = forms.CharField(widget=ReadonlyTextarea(), initial="A", label="Petition")
     giodo = forms.BooleanField(widget=forms.CheckboxInput(), required=True,
-        label="We want process you ID data. Do you accept it?")
+                               label="We want process you ID data. Do you accept it?")
+    newsletter = forms.BooleanField(widget=forms.CheckboxInput(), required=True,
+                                    label="Do you want receive newsletter?")
 
     def __init__(self, *args, **kwargs):
         super(SignatureForm, self).__init__(*args, **kwargs)
@@ -20,4 +38,5 @@ class SignatureForm(UserKwargModelFormMixin, ModelForm):
 
     class Meta:
         model = Signature
-        fields = ['name', 'email', 'city', 'telephone', 'location', 'lat', 'lng', 'giodo']
+        fields = ['recipient', 'petition', 'name', 'email', 'city',
+                  'telephone', 'location', 'lat', 'lng', 'giodo']
