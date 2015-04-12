45f8e3ce496abf0aab1a4f7bf5340a4421a2c7a5
Adam Dobrawy <naczelnik@jawnosc.tk>
2015-04-12 23:38:00 +0200

    Update urls of petition

--- ankieta/ankieta/petition/urls.py
+++ ankieta/ankieta/petition/urls.py
@@ -3,8 +3,8 @@ from django.conf.urls import patterns, url
 from . import views
 
 urlpatterns = patterns('',
-    url(r'^$', views.SignatureList.as_view(), name='list'),
-    url(r'^new/$', views.SignatureCreate.as_view(), name="create"),
+    url(r'^$', views.SignatureCreate.as_view(), name="create"),
+    url(r'^new/$', views.SignatureList.as_view(), name='list'),
     url(r'^thank-you/$', views.SignatureCreateDone.as_view(), name="thank-you"),
 
 )
