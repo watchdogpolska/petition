b946b1ed530c7a3302245f131d67c73b3439fa7a
Adam Dobrawy <naczelnik@jawnosc.tk>
2015-04-13 02:21:05 +0200

    Added maps

--- ankieta/petition/views.py
+++ ankieta/petition/views.py
@@ -12,13 +12,8 @@ class SignatureList(OrderableListMixin, AjaxResponseMixin, ListView):
     model = Signature
     orderable_columns = ("pk", "name", "city")
     orderable_columns_default = "created_on"
-    ajax_fields = ('pk', "name", "city", "lat", "lng")
     ordering = 'desc'
-
-    def get_paginate_by(self, *args, **kwargs):
-        if 'per_page' in self.request.GET and self.request.GET['per_page'].isdigit():
-            return int(self.request.GET['per_page'])
-        return 10
+    per_page = 10
 
     def get_context_data(self, **kwargs):
         context = super(SignatureList, self).get_context_data(**kwargs)
@@ -29,10 +24,23 @@ class SignatureList(OrderableListMixin, AjaxResponseMixin, ListView):
         qs = super(SignatureList, self).get_queryset(*args, **kwargs)
         return qs.visible()
 
-    def get_ajax(self, request, *args, **kwargs):
+
+class SignatureApiList(ListView):
+    model = Signature
+    ajax_fields = ('pk', "name", "city", "lat", "lng")
+
+    def get_paginate_by(self, *args, **kwargs):
+        if 'per_page' in self.request.GET and self.request.GET['per_page'].isdigit():
+            return int(self.request.GET['per_page'])
+        return 10
+
+    def get_queryset(self, *args, **kwargs):
+        qs = super(SignatureApiList, self).get_queryset(*args, **kwargs)
+        return qs.visible().location_full()
+
+    def get(self, request, *args, **kwargs):
         self.object_list = self.get_queryset()
-        context = self.get_context_data()
-        data = [dict((field, getattr(obj, field)) for field in self.ajax_fields) for obj in context['object_list']]
+        data = [dict((field, getattr(obj, field)) for field in self.ajax_fields) for obj in self.object_list]
         return JsonResponse(data, safe=False)
 
 
