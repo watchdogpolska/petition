9c0ce15a71229918d613b05ea06688de619ce843
Adam Dobrawy <naczelnik@jawnosc.tk>
2015-04-13 02:20:20 +0200

    Lat i lng non-required, added location_full

--- ankieta/petition/models.py
+++ ankieta/petition/models.py
@@ -1,11 +1,15 @@
 from django.db import models
 from django.db.models.query import QuerySet
+from django.db.models import Q
 
 
 class SignatureQuerySet(QuerySet):
 
     def visible(self):
         return self.filter(visible=True)
+
+    def location_full(self):
+        return self.exclude(Q(lat__isnull=True) & Q(lng__isnull=True))
 
 
 class Signature(models.Model):
@@ -14,8 +18,8 @@ class Signature(models.Model):
     city = models.CharField(max_length=100)
     location = models.CharField(max_length=150)
     telephone = models.CharField(max_length=12, null=True, blank=True)
-    lat = models.FloatField()
-    lng = models.FloatField()
+    lat = models.FloatField(null=True)
+    lng = models.FloatField(null=True)
     created_on = models.DateTimeField(auto_now_add=True)
     modified_on = models.DateTimeField(auto_now=True)
     visible = models.BooleanField(default=True)
