70f5a3fd7e28c574912a0318eba83f11789c2c7b
Adam Dobrawy <naczelnik@jawnosc.tk>
2015-04-21 01:36:09 +0200

    Fix URLS in contact form

--- ankieta/contact/urls.py
+++ ankieta/contact/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 urlpatterns = patterns('',
     url(r'^$', views.ContactView.as_view(), name="form"),
-    url(r'^success$', TemplateView.as_view(template_name="contact/success.html"), name="success"),
+    url(r'^/success$', TemplateView.as_view(template_name="contact/success.html"), name="success"),
 
 )
