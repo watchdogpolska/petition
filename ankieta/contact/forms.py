8b3c438b3f5fb9b2538a30182dd4f5d306aa098b
Adam Dobrawy <naczelnik@jawnosc.tk>
2015-04-21 00:31:15 +0200

    Fix contact form - send to recipient, not managers

--- ankieta/contact/forms.py
+++ ankieta/contact/forms.py
@@ -1,15 +1,22 @@
 from django import forms
-from django.core.mail import mail_managers
+from django.core.mail import send_mail
 from django.utils.translation import ugettext as _
 from django.core.urlresolvers import reverse
 from crispy_forms.helper import FormHelper
 from crispy_forms.layout import Submit
+from django.conf import settings
 from .models import Contact
 
 
+def my_mail_send(subject, recipient, message):
+    subject = '%s%s' % (settings.EMAIL_SUBJECT_PREFIX, subject)
+    from_email = settings.SERVER_EMAIL
+    return send_mail(subject, message, from_email, [recipient])
+
+
 class ContactForm(forms.Form):
-    personsList = forms.ModelChoiceField(required=True, label=_("Contact person"),
-                                         queryset=Contact.objects.all())
+    recipient = forms.ModelChoiceField(required=True, label=_("Contact person"),
+        queryset=Contact.objects.all())
     topic = forms.CharField(required=True, max_length=150,
         label=_("Topic of messages"))
     body = forms.CharField(required=True, widget=forms.Textarea(),  label=_("Content"))
@@ -26,4 +33,6 @@ class ContactForm(forms.Form):
         return "%(body)s \n\nE-mail: %(email)s" % self.cleaned_data
 
     def send(self):
-        mail_managers(self.cleaned_data['topic'], self.get_text())
+        my_mail_send(subject=self.cleaned_data['topic'],
+                     recipient=self.cleaned_data['recipient'].email,
+                     message=self.get_text())
