63a25fd7a13d40546e8f2fc4dedc517aae7a12eb
Adam Dobrawy <naczelnik@jawnosc.tk>
2015-04-25 03:29:31 +0200

    Packaged django-multi-contact

--- ankieta/contact/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import patterns, url
-from django.views.generic import TemplateView
-from . import views
-
-urlpatterns = patterns('',
-    url(r'^$', views.ContactView.as_view(), name="form"),
-    url(r'^/success$', TemplateView.as_view(template_name="contact/success.html"), name="success"),
-
-)
